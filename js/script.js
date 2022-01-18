add376158e741f12887cd08c179aa5d191dfcfed
urghanna1671 <97700783+urghanna1671@users.noreply.github.com>
2022-01-18 15:19:59 -0800

    first commit

--- js/script.js
+++ js/script.js
@@ -2,4 +2,8 @@
 /*dubel comments lovejv
 this day is very brightes*/
  console.log("hanna");
- console. log("my faverit food is egg")
+ console. log("my faverit food is egg");
+ 
+ console.log(("Supercalifragilisticexpialidocious").length);
+ console.log(7272727272727);
+ console.log(1000000000);
